cb99a4935281b7fb69ca7b0692fb96cd426423c1
Lim Chun Hoong <chunhoong.lim@outlook.my>
2019-08-08 01:03:35 +0800

    Create new exception when queried resource is absent.

---
+++ student-service/src/main/java/my/uni/services/studentsvc/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package my.uni.services.studentsvc.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+}
