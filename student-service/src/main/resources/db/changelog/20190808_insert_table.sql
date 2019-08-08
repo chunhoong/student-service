dd491e01da142c12d23bce8515beefbe521b023c
Lim Chun Hoong <chunhoong.lim@outlook.my>
2019-08-08 15:43:25 +0800

    Liquibase script to insert data.

---
+++ student-service/src/main/resources/db/changelog/20190808_insert_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO courses(
+    id, course_code, course_name, course_description
+)
+values
+    ("4e3c00e1-b934-11e9-816f-54ee75c3998a","TR2483","network engineering","<description>"),
+    ("81ff290e-b98f-11e9-816f-54ee75c3998a","TR2482","Computer engineering.","<description>"),
+    ("b537ed5e-b9ad-11e9-816f-54ee75c3998a","TR2484","Software architecture","<description>"),
+    ("b538033c-b9ad-11e9-816f-54ee75c3998a","TR2485","Software engineering","<description>")
